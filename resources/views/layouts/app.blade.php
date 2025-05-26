5f984d52ff7b933556f0e774553688eeb1921708
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-05-26 09:46:33 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,7 +87,7 @@
                             </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name ? Auth::user()->name : Auth::user()->full_name }}
+                                    {{ Auth::user()->name ? Auth::user()->name . Auth::user()->lname : Auth::user()->full_name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
