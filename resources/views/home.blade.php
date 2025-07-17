d6c72db76b22cb5ee7c75d032d8c90f95466f1fc
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-07-17 15:32:04 +0700

    fx

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            {{-- <div class="card shadow-sm mb-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                {{-- <div class="card shadow-sm mb-5">
                 <div class="card-body">
                     <p class="mb-0 pb-1 fs-5 border-bottom"><span><i class="bi bi-browser-edge"></i></span> Web Informations</p>
 
@@ -26,36 +26,63 @@
                     </div>
                 </div>
             </div> --}}
+                {{-- @if (request()->has('login_sso_error'))
+                    <div class="alert alert-danger">
+                        {{ request()->query('login_sso_error') }}
+                    </div>
+                @endif --}}
+                @if (request()->has('login_sso_error'))
+                    <div id="sso-alert" class="alert alert-danger" role="alert">
+                        {{ request()->query('login_sso_error') }}
+                    </div>
 
-            <div class="card shadow-sm">
-                <div class="card-body">
-                    <p class="mb-0 pb-1 fs-5 border-bottom"><span><i class="bi bi-star-fill"></i></span> Service</p>
+                    <script>
+                        setTimeout(function () {
+                            // Hide the alert
+                            const alert = document.getElementById('sso-alert');
+                            if (alert) {
+                                alert.style.display = 'none';
+                            }
 
-                    <div class="pt-3 container text-center">
-                        <div class="row row-cols-sm-2 row-cols-md-4">
-                            <a href="https://smarthub.trainingzenter.com/sso/login" class="link-underline link-underline-opacity-0">
-                                <div class="service p-2 rounded">
-                                    <img src="/imgs/logoiddrives.png" width="50" alt="">
-                                    <p class="mb-0">ระบบจัดการความรู้ออนไลน์</p>
-                                    <p class="mb-0">Hub-Training</p>
-                                </div>
-                            </a>
-                            <a href="https://checkbefore.trainingzenter.com/sso/{{ $user->username }}/{{ $user->prefix . ' ' . $user->name . ' ' . $user->lname }}/{{ $course_type }}/{{ $send_branch ?? 'idmskk' }}" target="_BLANK" class="link-underline link-underline-opacity-0">
-                                <div class="service p-2 rounded">
-                                    <img src="/imgs/logoiddrives.png" width="50" alt="">
-                                    <p class="mb-0">Check-Before Driving</p>
-                                </div>
-                            </a>
+                            // Remove query parameter from URL without reloading
+                            const url = new URL(window.location.href);
+                            url.searchParams.delete('login_sso_error');
+                            window.history.replaceState({}, document.title, url.pathname + url.search);
+                        }, 3000);
+                    </script>
+                @endif
+
+
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <p class="mb-0 pb-1 fs-5 border-bottom"><span><i class="bi bi-star-fill"></i></span> Service</p>
+
+                        <div class="pt-3 container text-center">
+                            <div class="row row-cols-sm-2 row-cols-md-4">
+                                <a href="http://127.0.0.1:8000/sso/login" class="link-underline link-underline-opacity-0">
+                                    <div class="service p-2 rounded">
+                                        <img src="/imgs/logoiddrives.png" width="50" alt="">
+                                        <p class="mb-0">ระบบจัดการความรู้ออนไลน์</p>
+                                        <p class="mb-0">Hub-Training</p>
+                                    </div>
+                                </a>
+                                <a href="https://checkbefore.trainingzenter.com/sso/{{ $user->username }}/{{ $user->prefix . ' ' . $user->name . ' ' . $user->lname }}/{{ $course_type }}/{{ $send_branch ?? 'idmskk' }}"
+                                    target="_BLANK" class="link-underline link-underline-opacity-0">
+                                    <div class="service p-2 rounded">
+                                        <img src="/imgs/logoiddrives.png" width="50" alt="">
+                                        <p class="mb-0">Check-Before Driving</p>
+                                    </div>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<style>
-    .service:hover {
-        background-color: #F0F0F0;
-    }
-</style>
+    <style>
+        .service:hover {
+            background-color: #F0F0F0;
+        }
+    </style>
 @endsection
--- routes/api.php
+++ routes/api.php
@@ -21,25 +21,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api', 'scope:view-user')->get('/user', function (Request $request) {
-    $courses_list = User_has_course::where('user_id', $request->user()->id)->orderBy('created_at', 'desc')->get(['course_id']);
     $agn = Agency::where('agn_id', $request->user()->agn)->first(['name', 'agn_id']);
     $brn = Branch::where('brn_id', $request->user()->brn)->first(['name', 'brn_id']);
     // $courses = App\Models\Course::whereIn('id', $courses_list)->get(['code', 'name']);
-    if ($request->user()->nationality == 'TH') {
-        $courses = DB::table('courses')
-                ->join('course_types', 'courses.course_type', '=', 'course_types.code')
-                ->select('courses.course_type', 'course_types.name')
-                ->where('courses.id', $courses_list[0]->course_id)
-                ->get();
-    } else {
-        $courseType = CourseType::where('code', "20240010")->get(['code', 'name']);
-        $courses = [
-            [
-                "course_type" => $courseType[0]->code,
-                "name" => $courseType[0]->name,
-            ]
 
-        ];
+    if ($request->user()->role !== 'staff') {
+        $courses_list = User_has_course::where('user_id', $request->user()->id)->orderBy('created_at', 'desc')->get(['course_id']);
+        if ($request->user()->nationality == 'TH') {
+            $courses = DB::table('courses')
+                    ->join('course_types', 'courses.course_type', '=', 'course_types.code')
+                    ->select('courses.course_type', 'course_types.name')
+                    ->where('courses.id', $courses_list[0]->course_id)
+                    ->get();
+        } else {
+            $courseType = CourseType::where('code', "20240010")->get(['code', 'name']);
+            $courses = [
+                [
+                    "course_type" => $courseType[0]->code,
+                    "name" => $courseType[0]->name,
+                ]
+
+            ];
+        }
     }
     $user_data = [
         "name" => ($request->user()->prefix ? $request->user()->prefix . ' ' : '') . $request->user()->name . ( $request->user()->lname ? (' ' . $request->user()->lname) : ''),
@@ -49,6 +52,7 @@ Route::middleware('auth:api', 'scope:view-user')->get('/user', function (Request
         "branch" => $brn ?? [],
         "agency" => $agn ?? [],
     ];
+
     return $user_data;
 });
 
