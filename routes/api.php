851d6777363475c72871755f9519db86271f236e
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2025-07-31 11:33:05 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,13 +22,9 @@ use Illuminate\Support\Facades\Route;
 
 /*
 {"name":"\u0e19\u0e32\u0e22 \u0e1e\u0e34\u0e17\u0e22\u0e32 \u0e2a\u0e38\u0e02\u0e14\u0e35",
-"username":"1409902897821",
-"role":"customer",
+"username":"1409902897821","role":"customer",
 "courses":[],
-"branch":{"name":"\u0e40\u0e14\u0e2d\u0e30\u0e42\u0e1b\u0e23\u0e40\u0e1f\u0e0a\u0e0a\u0e31\u0e48\u0e19\u0e41\u0e19\u0e25","brn_id":"IDD0006"},
-"agency":{"name":"\u0e2a\u0e16\u0e32\u0e1a\u0e31\u0e19\u0e44\u0e2d\u0e14\u0e35 \u0e44\u0e14\u0e23\u0e4c\u0e1f\u0e40\u0e27\u0e2d\u0e23\u0e4c",
-"agn_id":"IDD0002"}}
-
+"branch":{"name":"\u0e40\u0e14\u0e2d\u0e30\u0e42\u0e1b\u0e23\u0e40\u0e1f\u0e0a\u0e0a\u0e31\u0e48\u0e19\u0e41\u0e19\u0e25","brn_id":"IDD0006"},"agency":{"name":"\u0e2a\u0e16\u0e32\u0e1a\u0e31\u0e19\u0e44\u0e2d\u0e14\u0e35 \u0e44\u0e14\u0e23\u0e4c\u0e1f\u0e40\u0e27\u0e2d\u0e23\u0e4c","agn_id":"IDD0002"}}
 */
 
 Route::middleware('auth:api', 'scope:view-user')->get('/user', function (Request $request) {
@@ -64,7 +60,7 @@ Route::middleware('auth:api', 'scope:view-user')->get('/user', function (Request
         "name" => ($request->user()->prefix ? $request->user()->prefix . ' ' : '') . $request->user()->name . ( $request->user()->lname ? (' ' . $request->user()->lname) : ''),
         "username" => $request->user()->username,
         "role" => $request->user()->role,
-        "courses" => $courses ?? ["test"],
+        "courses" => ["test"],
         "branch" => $brn ?? [],
         "agency" => $agn ?? [],
     ];
